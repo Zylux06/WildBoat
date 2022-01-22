8eb9217a9c8c232777bc085062ab59fe04d8bc80
Zylux06 <vicenzo3verona@gmail.com>
2022-01-22 18:29:40 +0100

    new embed

--- index.js
+++ index.js
@@ -42,3 +42,21 @@ client.on("messageCreate", message => {
         canale.delete();
     }
 })
+
+client.on("messageCreate", message => {
+    if (message.content == "!comando") {
+        const embed = new Discord.MessageEmbed()
+            .setTitle("Titolo") //Titolo
+            .setColor("#34a42d") // Colore principale
+            .setURL("UrlTitolo") //Link sul titolo
+            .setAuthor("Autore") .setAuthor("Autore", "LinkImmagine") //Autore
+            .setDescription("Descrizione") //Descrizione
+            .setThumbnail("UrlCopertina") //Copertina
+            //Aggiungere elementi
+            .addField("Titolo", "Contenuto", true) //QUI TUTTI I PARAMETRI SONO OBBLIGATORI - True o false = se questo elemento deve essere in linea con gli altri
+            .setImage("LinkImmagine") //Immagine
+            .setFooter("TestoFooter") .setFooter("TestoFooter", "UrlImmagineFooter") // Testo piccolino in fondo
+            .setTimestamp() //Se mettere o no l'orario di arrivo del messaggio
+        message.channel.send({embeds: [embed]})
+    }
+})
