139a882dd5c0ff57926ef49fdaf73c1ebcb7f6ca
Zylux06 <vicenzo3verona@gmail.com>
2022-01-23 14:10:12 +0100

    bancommand

--- commands/assistenza.js
+++ commands/assistenza.js
@@ -4,6 +4,23 @@ module.exports = {
     name: "assistenza",
     description: "comando per l'assistenza",
     execute(message, args) {
-        message.channel.send("we");
+            var utente = message.mentions.members.first();
+            if (!message.member.permissions.has('BAN_MEMBERS')) {
+                return message.channel.send('Non hai il permesso');
+            }
+            if (!utente) {
+                return message.channel.send('Non hai menzionato nessun utente');
+            }
+            if (!utente.bannable) {
+                return message.channel.send('Io non ho il permesso');
+            }
+            utente.ban()
+                .then(() => {
+                    var embed = new Discord.MessageEmbed()
+                    .setTitle(`${utente.user.username} bannato`)
+                        .setDescription(`Utente bannato da ${message,author.toString()}`)
+    
+                    message.channel.send({ embeds: [embed] })
+                })
+        }
     }
-}
