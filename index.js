f5c3b7f3926ff8e93cb2c1f38271641211f57084
Zylux06 <vicenzo3verona@gmail.com>
2022-01-23 12:09:14 +0100

    avatarvia

--- index.js
+++ index.js
@@ -32,28 +32,3 @@ client.on("messageCreate", message => {
 
     client.commands.get(command).execute(message, args);
 })
-
-client.on("messageCreate", message => {
-    if (message.content.startsWith("!avatar")) {
-        if (message.content.trim() == "!avatar") {
-            var utente = message.member;
-        }
-        else {
-            var utente = message.mentions.members.first();
-        }
-        if (!utente) {
-            return message.channel.send("Utente non trovato")
-        }
-        var embed = new Discord.MessageEmbed()
-            .setColor("#00ff34")
-            .setTitle(utente.user.tag)
-            .setDescription("*Ecco l'avatar di questo utente!*")
-            .setImage(utente.user.displayAvatarURL({
-                dynamic: true,
-                format: "png",
-                size: 512
-            }))
-        message.channel.send({ embeds: [embed] })
-    }
-})
-
