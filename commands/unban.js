6443f9b79ac0dddf2386d45f6efff25c05751c46
Zylux06 <vicenzo3verona@gmail.com>
2022-01-24 13:21:43 +0100

    unban new

---
+++ commands/unban.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js")
+
+module.exports = {
+    name: "un-ban",
+    description: "comando di sban",
+    execute(message, args) {
+        if (!message.member.permissions.has('BAN_MEMBERS')) {
+            return message.channel.send('Non hai il permesso');
+        }
+
+        var args = message.content.split(/\s+/);
+        var idUtente = args[1]
+
+        if (!idUtente) {
+            return message.channel.send("Non hai scritto l'id di nessun utente");
+        }
+
+        message.guild.members.unban(idUtente)
+            .then(() => {
+                var embed = new Discord.MessageEmbed()
+                    .setTitle("Utente sbannato")
+                    .setDescription("Questo utente è stato sbannato")
+
+                message.channel.send({ embeds: [embed] })
+            })
+            .catch(() => { message.channel.send("Utente non valido o non bannato") })
+    }
+}
