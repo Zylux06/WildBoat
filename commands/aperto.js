1659f0aeb920a2d5f2a9e46e6636bc05593d0ea8
Zylux06 <vicenzo3verona@gmail.com>
2022-01-24 19:14:07 +0100

    new

--- commands/aperto.js
+++ commands/aperto.js
@@ -4,9 +4,6 @@ module.exports = {
     name: "aperto",
     description: "comunicare l'apertura dei bandi Staff!",
     execute(message, args) {
-        if (!message.member.permissions.has('MANAGE_MESSAGES')) {
-            return message.channel.send('Non hai il permesso');
-        }
 
     const embed = new Discord.MessageEmbed()
         .setTitle("Bandi Staff Aperti!") 
