567ca64a71d3b6b082eb7e629fee098adb707af1
Zylux06 <vicenzo3verona@gmail.com>
2022-01-24 22:29:09 +0100

    say

---
+++ commands/poll.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js")
+
+module.exports = {
+    name: "poll",
+    description: "fare un sondaggio",
+    execute(message, args) {
+            var args = message.content.split(/\s+/);
+            var testo;
+            testo = args.slice(1).join(" ");
+            if (!testo) {
+                return message.channel.send("Inserire un messaggio");
+            }
+            if (message.content.includes("@everyone") || message.content.includes("@here")) {
+                return message.channel.send("Non taggare everyone o here");
+            }
+            message.delete()
+    
+            //Messaggio classico
+            message.channel.send(testo)
+    
+            //Embed
+            var embed = new Discord.MessageEmbed()
+                .setTitle("Say")
+                .setDescription(testo)
+    
+            message.channel.send({embeds: [embed]})
+        }
+    }
