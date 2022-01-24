af56318e52ed622b2cc146f045917955b8406123
Zylux06 <vicenzo3verona@gmail.com>
2022-01-24 18:47:21 +0100

    new embed

---
+++ commands/aperto.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js")
+
+module.exports = {
+    name: "aperto",
+    description: "comunicare l'apertura dei bandi Staff!",
+    execute(message, args) {
+    if (message.content == "!aperto") {
+    const embed = new Discord.MessageEmbed()
+        .setTitle("Bandi Staff Aperti!") 
+        .setColor("RANDOM") 
+        .setAuthor("Los Angeles RP") 
+        .setDescription("Hey, i bandi staff sono aperti! Ti consigliamo di compilare il modulo con più informazioni possibili.") 
+        .setThumbnail("https://cdn.discordapp.com/icons/726687973176770560/a_04647115fc01336c6390dc1c63051b96.png?size=128") 
+        .setFooter("Bandi Staff Aperti") 
+        .setTimestamp() 
+    message.channel.send({embeds: [embed]})
+}
+   }}
