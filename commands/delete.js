35133a7a968c7ad7878e2142347ffe6c76243350
Zylux06 <vicenzo3verona@gmail.com>
2022-01-24 13:35:13 +0100

    delete

---
+++ commands/delete.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js")
+
+module.exports = {
+    name: "clear",
+    description: "cancellazione dei messaggi con delete",
+    execute(message, args) {
+            if (!message.member.permissions.has("MANAGE_MESSAGES")) {
+                return message.channel.send('Non hai il permesso');
+            }
+            if (!message.guild.me.permissions.has("MANAGE_MESSAGES")) {
+                return message.channel.send('Non ho il permesso');
+            }
+            var count = parseInt(message.content.split(/\s+/)[1]); 
+            if (!count) {
+                return message.channel.send("Inserisci un numero valido")
+            }
+            if (count > 100) {
+                return message.channel.send("Non puoi cancellare più di 100 messaggi")
+            }
+            message.channel.bulkDelete(count, true)
+            message.channel.send(count + " messaggi eliminati").then(msg => {
+                setTimeout(() => msg.delete(), 5000)
+        })
+    }
+}
