88602ac00489239b961bb61baa9d4924fa5692ee
Zylux06 <vicenzo3verona@gmail.com>
2022-01-24 21:32:15 +0100

    sup

---
+++ commands/sup.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js")
+
+module.exports = {
+    name: "sup",
+    description: "per richiedere assistenza tramite comando",
+    execute(message, args) {
+        
+    const embed = new Discord.MessageEmbed()
+        .setTitle("Richiesta Assistenza") 
+        .setColor("RANDOM") 
+        .setAuthor("Los Angeles RP") 
+        .setDescription("Hey, hai appena richiesto l'assistenza dello Staff, attendi... Ti chiediamo di non pingare lo Staff, inoltre inizia già ad esporre il problema!") 
+        .setThumbnail("https://cdn.discordapp.com/icons/726687973176770560/a_04647115fc01336c6390dc1c63051b96.png?size=128") 
+        .setFooter("Assistenza") 
+        .setTimestamp() 
+        message.channel.send({content: "@everyone", embeds: [embed]})
+    
+   }}
