d6757b5f106912aa514ff5be9026f341f5113176
Zylux06 <vicenzo3verona@gmail.com>
2022-01-25 11:01:46 +0100

    normale pol

--- commands/poll.js
+++ commands/poll.js
@@ -15,9 +15,6 @@ module.exports = {
                 return message.channel.send("Non taggare everyone o here");
             }
             message.delete()
-    
-            //Messaggio classico
-            message.channel.send(testo)
             
     
             //Embed
