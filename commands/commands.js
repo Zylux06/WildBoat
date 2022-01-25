bb4c2c4525eb0a6a90e588619d42318afc64e122
Zylux06 <vicenzo3verona@gmail.com>
2022-01-25 19:48:54 +0100

    add

--- commands/commands.js
+++ commands/commands.js
@@ -8,7 +8,7 @@ module.exports = {
         var embed = new Discord.MessageEmbed()
             .setTitle("Commands WildBot!")
             .setColor("RANDOM")
-            .setFields("w!aperto = Per aprire i bandi Staff, comando utilizzabile solo dallo Staff del Server.")
+            .addFields("w!aperto = Per aprire i bandi Staff, comando utilizzabile solo dallo Staff del Server.")
             message.channel.send({ embeds: [embed] })
 
     }}
