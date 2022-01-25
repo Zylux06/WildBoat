6083a7180aa1ca54d8c1dab1d6042e3e40cd2edf
Zylux06 <vicenzo3verona@gmail.com>
2022-01-25 19:42:33 +0100

    aaa

---
+++ commands/commands.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js")
+
+module.exports = {
+    name: "commands",
+    description: "lista dei comandi del bot Wild",
+    execute(message, args) {
+
+        var embed = new Discord.MessageEmbed()
+            .setTitle("Commands WildBot!")
+            .setColor("RANDOM")
+            .setFields("w!aperto = Per aprire i bandi Staff, comando utilizzabile solo dallo Staff del Server.")
+            message.channel.send({ embeds: [embed] })
+
+    }
+
+}
