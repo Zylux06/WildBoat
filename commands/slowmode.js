2a41e694f8ba421aeabbefaa918f114b90185129
Zylux06 <vicenzo3verona@gmail.com>
2022-01-25 15:04:35 +0100

    rrrr

--- commands/slowmode.js
+++ commands/slowmode.js
@@ -1,25 +1,36 @@
-const { ms } = require('discord.js')
+const Discord = require('discord.js')
 
 module.exports = {
-    name: "slowmode",
+    name: "role",
     description: "impostare lo slowmode in un canale",
     execute(message, args) {
 
-            var time = message.content.split(/\s+/)[1];
-            if (!time) {
-                return message.channel.send("Inserire un tempo valido")
+            var ruolo = message.mentions.roles.first()
+            if (!ruolo) {
+                return message.channel.send("Non ho trovato questo ruolo")
             }
-    
-            time = ms(time)
-            if (!time && time != 0) {
-                return message.channel.send("Inserire un tempo valido")
+            var memberCount = message.guild.members.cache.filter(member => member.roles.cache.find(role => role == ruolo)).size;
+            var permessiRuolo = new Discord.Permissions(ruolo.permissions.bitfield);
+            var elencoPermessi = "";
+            if (permessiRuolo.has("ADMINISTRATOR")) {
+                elencoPermessi = "👑ADMINISTRATOR";
             }
-    
-            if (time > 21600000) {
-                return message.channel.send("Inserire un tempo non superiore a 6 ore")
+            else {
+                var permessi = ["CREATE_INSTANT_INVITE", "KICK_MEMBERS", "BAN_MEMBERS", "MANAGE_CHANNELS", "MANAGE_GUILD", "ADD_REACTIONS", "VIEW_AUDIT_LOG", "PRIORITY_SPEAKER", "STREAM", "VIEW_CHANNEL", "SEND_MESSAGES", "SEND_TTS_MESSAGES", "MANAGE_MESSAGES", "EMBED_LINKS", "ATTACH_FILES", "READ_MESSAGE_HISTORY", "MENTION_EVERYONE", "USE_EXTERNAL_EMOJIS", "VIEW_GUILD_INSIGHTS", "CONNECT", "SPEAK", "MUTE_MEMBERS", "DEAFEN_MEMBERS", "MOVE_MEMBERS", "USE_VAD", "CHANGE_NICKNAME", "MANAGE_NICKNAMES", "MANAGE_ROLES", "MANAGE_WEBHOOKS"]
+                for (var i = 0; i < permessi.length; i++) {
+                    if (permessiRuolo.has(permessi[i])) {
+                        elencoPermessi += `- ${permessi[i]}\r`
+                    }
+                }
             }
-    
-            message.channel.setRateLimitPerUser(parseInt(time) / 1000)
-            message.channel.send("Slowmode impostata")
+            var embed = new Discord.MessageEmbed()
+                .setTitle(ruolo.name)
+                .setDescription("Tutte le statistiche di questo ruolo")
+                .addField("Role ID", ruolo.id, true)
+                .addField("Members", memberCount.toString(), true)
+                .addField("Color", ruolo.hexColor, true)
+                .addField("Role created", ruolo.createdAt.toDateString(), true)
+                .addField("Permissions", elencoPermessi, false)
+            message.channel.send({ embeds: [embed] })
         }
     }
