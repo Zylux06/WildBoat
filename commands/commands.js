4218c1795f042e87a2e70c0d54583946a2e0e6c4
Zylux06 <vicenzo3verona@gmail.com>
2022-01-25 20:00:22 +0100

    kick

--- commands/commands.js
+++ commands/commands.js
@@ -15,7 +15,7 @@ module.exports = {
             .addField('w!poll', 'Per fare un sondaggio')
             .addField('w!meteo', 'Dovrete scrivere il tempo della giornata, quindi come sarà la giornata')
             .addField('w!ban', 'Per bannare una persona dal Server Discord')
-            .addField('kick', 'Per espellere una persona sul Server Discord')
+            .addField('w!kick', 'Per espellere una persona sul Server Discord')
             .addField('w!unban', 'Per sbannare una persona dal Server Discord')
             .addField('w!delete', 'Per cancellare i messaggi prima del comando')
             .addField('w!commands', 'Per vedere tutta la lista completa dei comandi del bot')
