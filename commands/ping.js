ee3edafe1a4764c2980aa6befd587477a59d4670
Zylux06 <vicenzo3verona@gmail.com>
2022-01-26 22:18:27 +0100

    egr

---
+++ commands/ping.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js")
+
+module.exports = {
+    name: "ping",
+    description: "per vedere il ping del bot",
+    execute(message, args) {
+
+        var embed = new Discord.MessageEmbed()
+        .setTitle("Ping del bot")
+        .setDescription("Ecco la latenza del bot")
+        .addField("Ping", `${client.ws.ping}ms`)
+
+    message.channel.send({embeds: [embed]})
+    
+}}
