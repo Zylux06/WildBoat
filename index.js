3f770b39b54323dacb5be1182d90a9efd98954da
Zylux06 <vicenzo3verona@gmail.com>
2022-01-26 16:27:19 +0100

    ffvfff

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ client.on("guildMemberAdd", member => {
 // STATS MEMBRI DISCORD
 setInterval(function () {
     var canale = client.channels.cache.get("935916375636058152");
-    canale.setName(`👾│members: ${canale.guild.memberCount}`);
+    canale.setName(`Membri Totali: ${canale.guild.memberCount}`);
 }, 1000 * 60 * 5)
 
 const fs = require("fs");
