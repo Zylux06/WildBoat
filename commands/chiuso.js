d8197b19b5eb4a6bcc42dc914b42252afd863ec2
Zylux06 <vicenzo3verona@gmail.com>
2022-01-27 17:47:39 +0100

    chiusp

--- commands/chiuso.js
+++ commands/chiuso.js
@@ -1,18 +1,18 @@
 const Discord = require("discord.js")
 
 module.exports = {
-    name: "aperto",
-    description: "comunicare l'apertura dei bandi Staff!",
+    name: "chiuso",
+    description: "comunicare la chiusura dei bandi Staff!",
     execute(message, args) {
         if (!message.member.permissions.has('MANAGE_MESSAGES')) {
             return message.channel.send('**Hey, non hai il permesso per eseguire questo determinato comando! Solo lo staff del server può farlo.**');
         }
         
         var embed = new Discord.MessageEmbed()
-        .setTitle("‼️ Bandi Staff Aperti ‼️") 
-        .setColor("#ff0000") 
+        .setTitle("‼️ Bandi Staff Chiusi ‼️") 
+        .setColor("#ff7000") 
         .setAuthor("Los Angeles RP") 
-        .setDescription("__**Ragazzi, i bandi staff sono aperti! Affrettatevi a compilare e inviare il modulo, inoltre vi consigliamo di compilarlo con più informazioni possibili!**__") 
+        .setDescription("__**Mi dispiace annunciarvi che i bandi per entrare nello Staff sono attualmente chiusi.**__") 
         .setThumbnail("https://cdn.discordapp.com/emojis/838709960719990834.webp?size=96&quality=lossless") 
         .setFooter("Bandi Staff") 
         .setTimestamp() 
