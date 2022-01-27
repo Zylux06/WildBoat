2d72aafe4a6fa2fbdc277c0d4b1c655333aa1b76
Zylux06 <vicenzo3verona@gmail.com>
2022-01-27 19:31:07 +0100

    a

--- commands/bug.js
+++ commands/bug.js
@@ -5,7 +5,7 @@ module.exports = {
     description: "per inviare un bug all'interno del bot",
     execute(message, args) {
 
-        message.channel.send(`${message.author.toString()} Il Bug è stato inviato correttamente nel canale dei bug, grazie per avercelo segnalato!!`)
+        message.channel.send(`*${message.author.toString()} Il Bug è stato inviato correttamente nel canale dei bug, grazie per averci segnalato il bug, presto provvederemo a risolverlo!*`)
 
         var args = message.content.split(/\s+/);
         var testo;
@@ -20,10 +20,10 @@ module.exports = {
         const embed = new Discord.MessageEmbed()
             .setTimestamp("Bug Rilevato!")
             .setColor("RANDOM")
-            .setDescription(`**${message.author.toString()} ha rilevato questo butg:\n${testo}**`)
+            .setDescription(`***${message.author.toString()} Ha rilevato questo bug:*** **\n${testo}**`)
             .setThumbnail("https://cdn.discordapp.com/attachments/935655567526068264/935881520596533258/template-sticker-600x600.png")
-            .setImage("https://cdn.discordapp.com/attachments/931126999970361344/935877896378142770/sole-min-scaled-e1610018813683.png")
-            .setFooter("Bug")
+            .setImage("https://cdn.discordapp.com/attachments/935843042391371827/936315614425907281/88-512.png")
+            .setFooter("Bug Rilevato!")
             .setTimestamp()
 
             client.channels.cache.get("936316266904436806").send({embeds: [embed]})
