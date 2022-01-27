4f7f01f9235ff9de47a921583441eb1350ebd9c4
Zylux06 <vicenzo3verona@gmail.com>
2022-01-27 19:04:19 +0100

    das

--- commands/bug.js
+++ commands/bug.js
@@ -5,7 +5,17 @@ module.exports = {
     description: "per inviare un bug all'interno del bot",
     execute(message, args) {
 
-    var testo;
+        var args = message.content.split(/\s+/);
+        var testo;
+        testo = args.slice(1).join(" ");
+        if (!testo) {
+            return message.channel.send("Inserire un Messaggio");
+        }
+        if (message.content.includes("@everyone") || message.content.includes("@here")) {
+            return message.channel.send("Non taggare everyone o here");
+        }
+        message.delete()
+
     const embed = new Discord.MessageEmbed()
         .setTitle("Bug Rilevato!") 
         .setColor("RANDOM") 
