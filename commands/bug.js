213ec7da4f2653116ef894870f56f2c5115ba0ec
Zylux06 <vicenzo3verona@gmail.com>
2022-01-27 18:44:32 +0100

    bug

---
+++ commands/bug.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js")
+
+module.exports = {
+    name: "bug",
+    description: "per inviare un bug all'interno del bot",
+    execute(message, args) {
+
+    const embed = new Discord.MessageEmbed()
+        .setTitle("Bug Rilevato!") 
+        .setColor("RANDOM") 
+        .setDescription(`**${message.author.toString()} ha rilevato questo bug:\n${testo}**`)
+        .setThumbnail("https://cdn.discordapp.com/attachments/935655567526068264/935881520596533258/template-sticker-600x600.png")
+        .setImage("https://cdn.discordapp.com/attachments/935843042391371827/936315614425907281/88-512.png")
+        .setFooter("Bug")
+        .setTimestamp()
+
+        message.channel.send({ embeds: [embed] })
+    
+   }}
