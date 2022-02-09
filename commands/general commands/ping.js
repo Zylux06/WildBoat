2729fc91513d8a89ee32871594adf4cb3536d7fb
Zylux06 <vicenzo3verona@gmail.com>
2022-02-09 19:23:11 +0100

    sss

--- commands/general commands/ping.js
+++ commands/general commands/ping.js
@@ -9,6 +9,7 @@ module.exports = {
         .setTitle("⭐__Ping Del Bot!__⭐")
         .setColor("RANDOM")
         .setDescription("**Ecco la latenza del bot!**")
+        .addField('__Info:__', 'La latenza, indica in un sistema di elaborazione dati e o di telecomunicazioni, l intervallo di tempo che intercorre fra il momento in cui viene inviato l input - segnale al sistema e il momento in cui è disponibile il suo output. In altre parole, la latenza non è altro che una misura della velocità di risposta di un sistema.')
         .addField("__Ping:__", `**${client.ws.ping} Ms**`)
         .setThumbnail("https://cdn.discordapp.com/attachments/935655567526068264/935881520596533258/template-sticker-600x600.png")
         .setFooter({text: "Ping Bot"})
