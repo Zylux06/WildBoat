bcc0c8b372f96977d861c5d92fa2039dc204ade8
Zylux06 <vicenzo3verona@gmail.com>
2022-02-09 20:08:24 +0100

    ddd

---
+++ commands/general commands/user-info.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js")
+
+module.exports = {
+    name: "server-info",
+    description: "per vedere le informazioni su un server",
+    execute(message, args) {
+
+        var server = message.guild;
+
+        var embed = new Discord.MessageEmbed()
+            .setTitle(server.name)
+            .setDescription("Tutte le info su questo server")
+            .setThumbnail(server.iconURL())
+            .addField("Owner", client.users.cache.get(server.ownerId).username, true)
+            .addField("Server id", server.id, true)
+            .addField("Members", server.memberCount.toString(), false)
+            .addField("Channels", server.channels.cache.size.toString(), false)
+            .addField("Server created", server.createdAt.toDateString(), true)
+            .addField("Boost level", "Level " + (server.premiumTier != "NONE" ? server.premiumTier : 0) + " (Boost: " + server.premiumSubscriptionCount + ")", true)
+        message.channel.send({ embeds: [embed] })
+    }
+}
