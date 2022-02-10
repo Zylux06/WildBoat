0566eb92e053cd256be36a54798eeb32a955dd10
Zylux06 <vicenzo3verona@gmail.com>
2022-02-10 15:28:13 +0100

    assa

---
+++ commands/general commands/bottoni.js
@@ -0,0 +1,23 @@
+const { MessageActionRow, MessageButton, DiscordAPIError } = require('discord.js');
+
+module.exports = {
+    name: "bottoni",
+    description: "bottoni discord",
+    execute(message, args) {
+
+        let row = new MessageActionRow()
+			.addComponents(
+				new MessageButton()
+					.setCustomId('primary')
+					.setLabel('Primary')
+					.setStyle('PRIMARY')
+            )
+                var embed = new Discord.MessageEmbed()    
+                    .setTitle("⭐__Buttons__⭐")
+                    .setColor("RANDOM")
+                    .setDescription("Ciao")
+
+
+                message.channel.send({embeds: [embed], components: [row]})
+
+            }}         
