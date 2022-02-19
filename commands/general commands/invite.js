f807dca6c29c54d4a77f2b1068c4d6783e41eb30
Zylux06 <vicenzo3verona@gmail.com>
2022-02-19 20:04:23 +0100

    dsa

--- commands/general commands/invite.js
+++ commands/general commands/invite.js
@@ -10,7 +10,7 @@ module.exports = {
         var embed = new Discord.MessageEmbed()
             .setTitle("🌏__Invito del Bot!__🌏")
             .setColor("RANDOM")
-            .setDescription(`**${message.author.toString()} Ecco a te il Link per invitare il bot nel tuo bellissimo Server! 😜 \n https://discord.com/api/oauth2/authorize?client_id=934115799877161010&permissions=8&scope=bot***`)
+            .setDescription(`***${message.author.toString()} Ecco a te il Link per invitare il bot nel tuo bellissimo Server! 😜 \n https://discord.com/api/oauth2/authorize?client_id=934115799877161010&permissions=8&scope=bot***`)
             .setURL("https://discord.com/api/oauth2/authorize?client_id=934115799877161010&permissions=8&scope=bot")
             .setFooter({text: "Invito al Bot"})
             .setTimestamp()
