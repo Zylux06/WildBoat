cac1a20f8ee96a8251072ce185d42c6de255dde8
Zylux06 <vicenzo3verona@gmail.com>
2022-02-19 19:22:15 +0100

    dsaasd

---
+++ commands/staff commands/warn.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js")
+
+module.exports = {
+    name: "warn",
+    description: "per warnare un utente",
+    execute(message, args) {
+
+        if (!message.member.permissions.has('')) {
+            return message.channel.send('MANAGE_MESSAGE');
+        }
+
+        var testo;
+        testo = args.join(" ");
+        if (!testo) {
+            return message.channel.send("Inserire un Messaggio");
+        }
+
+        message.delete()
+
+        const UtenteWarnato = message.mentions.members.first()
+
+        var embed = new Discord.MessageEmbed()
+            .setTitle("⚠__Infrazione!__⚠")
+            .setColor("RED")
+            .addField('Moderatore:', `${message.author.toString()}`)
+            .addField('Warnato:', `${UtenteWarnato}`)
+            .addField('Motivazione:', `${testo}`)
+
+            message.channel.send({ embeds: [embed] })
+
+    }
+}                
+
