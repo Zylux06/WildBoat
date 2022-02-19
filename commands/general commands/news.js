fce757a3448183fb4e2711270e934b98c14dfba6
Zylux06 <vicenzo3verona@gmail.com>
2022-02-19 18:50:13 +0100

    ddasads

---
+++ commands/general commands/news.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js")
+
+module.exports = {
+    name: "news",
+    description: "per mandare una news telegiornale",
+    execute(message, args) {
+
+        var testo;
+        testo = args.slice(1).join(" ");
+        if (!testo) {
+            return message.channel.send("Inserire un Messaggio");
+        }
+        message.delete()
+
+        var embed = new Discord.MessageEmbed()
+            .setTitle("🗓__Telegiornale__🗓")
+            .setColor("RANDOM")
+            .setDescription("**Nuova Edizione del TG Fox!**")
+            .addField('Conduttore:', `${message.author.toString()}`)
+            .addField('Notizia:', `${testo}`)
+            .setFooter({text: "TG Fox"})
+            .setTimestamp()
+
+            message.channel.send({content: "@everyone", embeds: [embed]})
+
+    }
+}                
