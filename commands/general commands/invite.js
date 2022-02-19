a9ad34a734cd1446c75b90c3eb23f0d65c7b776a
Zylux06 <vicenzo3verona@gmail.com>
2022-02-19 19:55:11 +0100

    ss

---
+++ commands/general commands/invite.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js")
+
+module.exports = {
+    name: "invite",
+    description: "per mandare il link del bot pubblico",
+    execute(message, args) {
+
+        var embed = new Discord.MessageEmbed()
+            .setTitle("🌏__Invito del Bot!__🌏")
+            .setColor("RANDOM")
+            .setDescription(`${message.author.toString()} Ecco a te il Link per invitare il bot nel tuo bellissimo Server! 😜`)
+            .addField('➔', 'https://discord.com/api/oauth2/authorize?client_id=934115799877161010&permissions=8&scope=bot')
+            .setURL("https://discord.com/api/oauth2/authorize?client_id=934115799877161010&permissions=8&scope=bot")
+            .setFooter({text: "Invito al Bot"})
+            .setTimestamp()
+
+            message.channel.send({ embeds: [embed] })
+
+    }}            
