68188d2e8d5b01439d05831ad6c609b66a9c1bb2
Zylux06 <vicenzo3verona@gmail.com>
2022-02-22 16:25:56 +0100

    fd

--- commands/general commands/sup.js
+++ commands/general commands/sup.js
@@ -10,7 +10,7 @@ module.exports = {
     const embed = new Discord.MessageEmbed()
         .setTitle("Richiesta Assistenza!") 
         .setColor("#fc6b03") 
-        .setAuthor("Los Angeles RP") 
+        .setAuthor("Assistenza") 
         .setDescription(`**Hey ${message.author.toString()}, hai appena richiesto l'assistenza dello Staff, attendi... Ti chiediamo di non pingare lo Staff, inoltre inizia già ad esporre il problema!**`) 
         .setThumbnail("https://cdn.discordapp.com/attachments/935843968665669683/943921059831377940/1200px-Circle-icons-tools.png") 
         .setFooter("Assistenza") 
