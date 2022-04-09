1cee92faac329351756a04a9803eb2b9bd510671
Zylux06 <vicenzo3verona@gmail.com>
2022-04-09 17:32:52 +0200

    ss

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ client.on("interactionCreate", Interaction => {
     if  (!interaction.isCommand()) return
 
     if  (interaction.commandName == "ping") {
-        interaction.reply("Pongs!!")
+        interaction.reply("Pong!!")
     }
 })
 
