bab7472ee001c2e21e6a5379650638b237669b46
Zylux06 <vicenzo3verona@gmail.com>
2022-04-10 19:17:57 +0200

    fs

---
+++ commands/general commands/me.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js")
+
+module.exports = {
+    name: "me",
+    description: "messaggio normale",
+    execute(message, args) {
+
+        var args = message.content.split(/\s+/);
+        var testo;
+        testo = args.slice(1).join(" ");
+        if (!testo) {
+            return message.channel.send("Inserire un Messaggio");
+        }
+        if (message.content.includes("@everyone") || message.content.includes("@here")) {
+            return message.channel.send("Non taggare everyone o here");
+        }
+        message.delete()
+
+        var embed = new Discord.MessageEmbed()
+            .setTitle("**__🏃🏻‍♂️Azione RP🏃🏻‍♂️__**")
+            .setColor("RANDOM")
+            .setDescription(`*${message.author.toString()} = ${testo}*`)
+            .setTimestamp()
+
+            message.channel.send({ embeds: [embed] })
+    
+        }}
