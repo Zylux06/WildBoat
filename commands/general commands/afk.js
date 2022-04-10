4906d75c5a51bd02d8b106df92284e2de8401979
Zylux06 <vicenzo3verona@gmail.com>
2022-04-10 15:15:07 +0200

    da

--- commands/general commands/afk.js
+++ commands/general commands/afk.js
@@ -5,19 +5,12 @@ module.exports = {
     description: "mettersi afk",
     execute(message, args) {
 
-        var testo;
-        testo = args.join(" ");
-        if (!testo) {
-            return message.channel.send("Inserire un Messaggio");
-        }
-
         message.delete()
 
         var embed = new Discord.MessageEmbed()
             .setColor("RANDOM")
             .setTitle("😴__Utente AFK__😴")
             .addField('Utente AFK:', `${message.author.toString()}`)
-            .addField('Motivo:', `${testo}`)
             .setFooter({text: "Utente Andato AFK"})
             .setImage(utente.user.displayAvatarURL({
                 dynamic: true,
