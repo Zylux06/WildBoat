447238a3b16c6a776ca0d689677bca7ba4dc8213
Zylux06 <vicenzo3verona@gmail.com>
2022-04-11 19:05:46 +0200

    adsads

---
+++ commands/staff commands/carica.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js")
+
+module.exports = {
+    name: "carica",
+    description: "per prendersi in carica un Ticket",
+    execute(message, args) {
+
+        message.delete()
+
+    var embed = new Discord.MessageEmbed()
+        .setTitle("__📩**Ticket Preso In Carica**📩__")
+        .setColor("RANDOM")
+        .setThumbnail("https://cdn.discordapp.com/attachments/935843968665669683/963122530833539142/unknown.png")
+        .setDescription(`Il Ticket è stato preso in carica da ${message.author.toString()}!`)
+        .setAuthor(`${message.author.toString()}`)
+        .setFooter({text: "Ticket"})
+        .setTimestamp()
+        
+        message.channel.send({ embeds: [embed] })
+    }}
