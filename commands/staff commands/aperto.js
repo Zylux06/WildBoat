42774e84608ad1584172f02eb36e103784afe614
Zylux06 <vicenzo3verona@gmail.com>
2022-04-15 10:54:09 +0200

    d

--- commands/staff commands/aperto.js
+++ commands/staff commands/aperto.js
@@ -14,7 +14,7 @@ module.exports = {
         var embed = new Discord.MessageEmbed()
         .setTitle("‼Bandi Staff Aperti") 
         .setColor("RANDOM") 
-        .setDescription("__**<:Annuncio_Wild:964447174219956254> Hey Ragazzi, i bandi staff sono aperti! Affrettatevi a compilare e inviare il modulo, inoltre vi consigliamo di compilarlo con più informazioni possibili!**__") 
+        .setDescription("__**Hey Ragazzi, i bandi staff sono aperti! Affrettatevi a compilare e inviare il modulo, inoltre vi consigliamo di compilarlo con più informazioni possibili!**__") 
         .setImage("https://cdn.discordapp.com/attachments/935843412056350730/944648012649619507/closing-20200214112654660.png") 
         .setFooter("Bandi Staff") 
         .setTimestamp() 
