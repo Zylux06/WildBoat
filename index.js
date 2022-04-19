023cfde88eb12df6ddf944de7c969f9e25544a70
Zylux06 <vicenzo3verona@gmail.com>
2022-04-19 13:23:08 +0200

    metti qualcosa qui haha

--- index.js
+++ index.js
@@ -57,16 +57,17 @@ client.on("messageCreate", message => {
     const args = message.content.slice(prefix.length).trim().split(/ +/);
     const command = args.shift().toLocaleLowerCase();
 
-    if (!client.commands.has(command)) {
+    if (!client.commands.has(command)) return
+    
+
+    client.commands.get(command).execute(message, args);
+
+    const cmd = client.commands.get(command) 
+    if (!cmd) {
         var embed = new Discord.MessageEmbed()
             .setColor("#ff0000")
             .setTitle("Comando non esistendte")
             .setDescription("Il comando non esiste!" )
         return message.channel.send(embed)
     }
-    
-
-    client.commands.get(command).execute(message, args);
-
-    const cmd = client.commands.get(command) 
 })
