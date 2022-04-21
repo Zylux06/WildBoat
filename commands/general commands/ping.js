267aa7c6409bff67cd6bd9ee195f586e932eadd7
Zylux06 <vicenzo3verona@gmail.com>
2022-04-21 18:20:24 +0200

    dfas

--- commands/general commands/ping.js
+++ commands/general commands/ping.js
@@ -11,7 +11,7 @@ module.exports = {
         .setDescription("**Ecco la latenza del bot!**")
         .addField('__Info:__', '***La latenza, indica in un sistema di elaborazione dati e o di telecomunicazioni, l intervallo di tempo che intercorre fra il momento in cui viene inviato l input - segnale al sistema e il momento in cui è disponibile il suo output. In altre parole, la latenza non è altro che una misura della velocità di risposta di un sistema.***')
         .addField("__Ping:__", `**${client.ws.ping} Ms**`)
-        .setFooter({text: "Ping Bot"})
+        .setFooter({text: "Ping Bot!"})
         .setTimestamp()
 
     message.channel.send({embeds: [embed]})
