83c15e0f9986ca4ab4ae483c2005f4aca2445346
Zylux06 <vicenzo3verona@gmail.com>
2022-06-11 17:42:13 +0200

    re

--- commands/general commands/poll.js
+++ commands/general commands/poll.js
@@ -21,7 +21,7 @@ module.exports = {
                 .setTitle("Sondaggio!")
                 .setThumbnail("https://cdn.discordapp.com/attachments/935843412056350730/944647164913680414/architecture-5928534_960_720.png")
                 .setDescription(`**${message.author.toString()} Ha scritto il messaggio:\n${testo}**`)
-                .setFooter({text: "Sondaggio Server"})
+                .setFooter({text: "Sondaggio Server!"})
                 .setTimestamp()
     
             message.channel.send({embeds: [embed]})
